fd1a36fb5c1bdffb48070869e70ca82c8c573b11
AIsoltani <amirsoltani_os@hotmail.com>
2023-04-27 09:31:39 -0700

    Add files via upload

---
+++ Perceptron_Linear_Regression.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Loading dataset from pandas lib
+
+x = pd.read_csv("spambase_X.csv",header=None)
+y = pd.read_csv("spambase_y.csv",header=None)
+x = np.array(x)
+y = np.array(y)
+y = np.reshape(y,-1)
+print(x)
+print(y)
+n,d = x.shape
+mistake = np.zeros(500)
+w = np.zeros(d)
+b = 0
+iteration=500
+
+for i in range(iteration):
+    Mistake = 0
+    for j in range(n):
+        if (y[j] * (np.dot(w.T,x[j,:]) + b) <= 0):
+            w = w + y[j]*x[j,:]
+            b = b + y[j]
+            Mistake = Mistake + 1   
+    mistake[i] = Mistake
+    
+plt.figure()
+plt.plot(range(iteration), mistake)
+plt.xlabel('Number of Passes')
+plt.ylabel('Number of Mistakes')
+plt.show()
